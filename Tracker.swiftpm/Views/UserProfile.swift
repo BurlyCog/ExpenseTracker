0afdc7939b888ab29121acd25166e4632f55b99b
Burly.Ben <96114095+BurlyCog@users.noreply.github.com>
2025-04-27 16:29:51 +0530

    Delete Tracker.swiftpm directory
    retry upload

--- Tracker.swiftpm/Views/UserProfile.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-class UserProfile: ObservableObject {
-    @Published var email: String = ""
-    @Published var username: String = ""
-    @Published var profileImage: UIImage? = nil
-}
